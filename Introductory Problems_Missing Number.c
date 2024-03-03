bd15fd9e7bdc6b6847984f027d4a97317e977324
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-03-04 04:24:31 +0700

    Update Introductory Problems_Missing Number.c

--- Introductory Problems_Missing Number.c
+++ Introductory Problems_Missing Number.c
@@ -1,20 +1,12 @@
 //Missing Number
 #include <stdio.h>
+long long sum, N, arr[2000000] ;
 
 int main() {
-    int N, arr[200000];
-    scanf("%d", &N);
-    for(int i = 0; i < N-1; i++)
-        scanf("%d", &arr[i]);
-    for(int i = 1; i < N; i++){
-        int count = 0;
-        for(int j = 0; j < N-1; j++){
-            if(i==arr[j])
-                count++;
-        }
-        if(count==0){
-            printf("%d", i);
-            break;
-        }
-    }
+    scanf("%lld", &N);
+    for(int i = 0; i < N-1; i++){
+        scanf("%lld", &arr[i]);
+		sum += arr[i];
+	}
+    printf("%lld", N*(N+1)/2 - sum);
 }
