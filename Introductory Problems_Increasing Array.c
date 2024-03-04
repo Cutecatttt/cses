fffa3151e0999dbf9152385d39b81cdede9c591e
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-03-04 15:02:02 +0700

    Update Introductory Problems_Increasing Array.cpp

--- Introductory Problems_Increasing Array.c
+++ Introductory Problems_Increasing Array.c
@@ -1,15 +1,17 @@
 //Increasing Array
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+long long N, arr[200000];
+using namespace std;
 
 int main() {
-    int N, arr[200000];
     scanf("%d", &N);
-    int count = 0;
+    long long count = 0;
     for(int i = 0; i < N; i++){
         scanf("%d", &arr[i]);
-        if(arr[i] < arr[i-1])
-            count += (arr[i-1] - arr[i]); 
+        if(arr[i] < arr[i-1]){
+            count += (arr[i-1] - arr[i]);
+			arr[i] = arr[i-1];
+		}			
     }
-    printf("%d", count);
+    printf("%lld", count);
 }
