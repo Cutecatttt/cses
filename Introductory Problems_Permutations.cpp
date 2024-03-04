19fc9981e71d4da1f42f47183da48b4385276c56
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-03-04 22:19:21 +0700

    Update and rename Introductory Problems_Permutations.c to Introductory Problems_Permutations.cpp

--- Introductory Problems_Permutations.cpp
+++ Introductory Problems_Permutations.cpp
@@ -1,14 +1,15 @@
 //Permutations
-#include <stdio.h>
-
+#include <iostream>
+#include <string>
+#include <cstring>
 int main() {
     int N;
     scanf("%d", &N);
     if(N == 3||N == 2) printf("NO SOLUTION");
     else{
-        for(int i = 1; i< N+1; i+=2)
-            printf("%d ", i);
         for(int i = 2; i< N+1; i+=2)
+            printf("%d ", i);
+        for(int i = 1; i< N+1; i+=2)
             printf("%d ", i);
     }
 }
