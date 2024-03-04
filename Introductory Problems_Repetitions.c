9529c8e34ff3215cd84c439ee8cb8024e921e8a9
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-03-04 14:55:07 +0700

    Update Introductory Problems_Repetitions.cpp

--- Introductory Problems_Repetitions.c
+++ Introductory Problems_Repetitions.c
@@ -1,11 +1,14 @@
 //Repetitions
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
 int main() {
     char str[1000000];
-    int max = 0;
+    int max = 1;
     int count = 1;
-    scanf("%s", &str);
+    scanf("%s", str);
     for(int i = 1; i < strlen(str); i++){
         if(str[i] == str[i-1]){
             count++;
