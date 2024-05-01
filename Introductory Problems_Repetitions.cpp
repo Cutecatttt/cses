1205dc94d308490103437d1cfe3560c3d5a987ca
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-05-01 23:38:20 +0700

    Update and rename Introductory Problems_Repetitions.c to Introductory Problems_Repetitions.cpp

--- Introductory Problems_Repetitions.cpp
+++ Introductory Problems_Repetitions.cpp
@@ -1,20 +1,20 @@
-//Repetitions
-#include <iostream>
-#include <string>
-#include <cstring>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+long long sum, N, arr[2000000] ;
+
 int main() {
-    char str[1000000];
+    string str;
     int max = 1;
     int count = 1;
-    scanf("%s", str);
-    for(int i = 1; i < strlen(str); i++){
+    cin >> str;
+    for(int i = 1; i < size(str); i++){
         if(str[i] == str[i-1]){
             count++;
             if (count > max) max = count;
         }
         else count = 1;
     }
-    printf("%d", max);
+    cout << max;
 }
