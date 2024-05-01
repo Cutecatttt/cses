998549786ead88cd5afe3f6d268ecb5a8b318f2b
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-05-01 23:44:57 +0700

    Update Introductory Problems_Permutations.cpp

--- Introductory Problems_Permutations.cpp
+++ Introductory Problems_Permutations.cpp
@@ -1,15 +1,14 @@
-//Permutations
-#include <iostream>
-#include <string>
-#include <cstring>
+#include <bits/stdc++.h>
+
+using namespace std;
+
 int main() {
-    int N;
-    scanf("%d", &N);
+    int N; cin >> N;
     if(N == 3||N == 2) printf("NO SOLUTION");
     else{
         for(int i = 2; i< N+1; i+=2)
-            printf("%d ", i);
+            cout << i << " ";
         for(int i = 1; i< N+1; i+=2)
-            printf("%d ", i);
+            cout << i << " ";
     }
 }
