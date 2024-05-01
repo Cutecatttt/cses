baa2b7f6ad095ae5ff3d9f7ddab73624e103cafd
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-05-01 23:52:13 +0700

    Update Introductory Problems_Number Spiral.cpp

--- Introductory Problems_Number Spiral.cpp
+++ Introductory Problems_Number Spiral.cpp
@@ -1,30 +1,27 @@
-//Number Spiral
-#include <iostream>
-#include <string>
-#include <cstring>
+#include <bits/stdc++.h>
+
 using namespace std;
+
 int main() {
-    long long t;
-    scanf("%lld", &t);
+    long long t; cin >> t;
     long long x[t], y[t];
     for(int m = 0; m < t; m++)
-        scanf("%lld%lld", &x[m], &y[m]);
+        cin >> x[m] >> y[m];
     for(int m = 0; m < t; m++){   
         if(x[m] >= y[m]){
             long long S = (x[m]-1)*(x[m]-1);
             if(x[m]%2 == 0)
-                printf("%lld ", S + 2*x[m] - y[m]);
+                cout << S + 2*x[m] - y[m] << endl;
             else
-                printf("%lld ", S + y[m]);
+                cout << S + y[m] << endl;
         }
         else{
             long long S = (y[m]-1)*(y[m]-1);
             if(y[m]%2 == 1)
-                printf("%lld ", S + 2*y[m] - x[m]);
+                cout << S + 2*y[m] - x[m] << endl;
             else
-                printf("%lld ", S + x[m]);
+               cout << S + x[m] << endl;
         }
     }
-
     return 0;
 }
